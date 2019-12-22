f007bbbac6f671d730510573163854b0b474f92a
Jakub Rękas <jakubrekas1@gmail.com>
2019-12-22 10:20:51 +0100

    Install dependencies; Example of use booking widget;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import BookingWidget from '@codegraphy/booking-widget';
 
 function App() {
+  const [reservedDates, setReservedDates] = useState([]);
+
+  const handleSubmit = (data) => {
+    // do something...
+    console.log(data);
+  };
+
+  // Fetch example data
+  useEffect(() => {
+    const data = [
+      '2019-12-06 00:00:00',
+      '2019-12-07 00:00:00',
+      '2019-12-08 00:00:00',
+      '2019-12-16 00:00:00',
+      '2019-12-24 00:00:00',
+      '2019-12-25 00:00:00',
+      '2019-12-26 00:00:00',
+    ];
+
+    setReservedDates(data);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BookingWidget
+          amount={100}
+          currency="zł"
+          lastUpdateDays={15}
+          onSubmit={handleSubmit}
+          rate={2.5}
+          reservedDates={reservedDates}
+          votes={123}
+        />
       </header>
     </div>
   );
